214f16b65b6a6dc7922a72588fb6e255264c8c75
hdsnbl <baileyhuds@gmail.com>
2024-03-02 19:30:42 -0600

    Created the shared resources and added them to the MainPage.xaml

---
+++ exercise1/Notes/SharedResources.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Notes;
+
+static class SharedResources
+{
+    public static readonly Color FontColor = Color.FromRgb(0, 0, 0xFF);
+    public static readonly Color BackgroundColor = Color.FromRgb(0xFF, 0xF0, 0xAD);
+
+}
